adeb037b879cc671b743c512f125bd2f898c019e
Martin Joehren <m.joehren@gmail.com>
2014-06-16 23:01:28 +0200

    fixed marshaller

--- src/main/scala/de.flapdoodle.server/AdminHttpServerRoutes.scala
+++ src/main/scala/de.flapdoodle.server/AdminHttpServerRoutes.scala
@@ -5,25 +5,25 @@ import akka.pattern.ask
 import spray.can._
 import spray.can.server.{Stats => SprayStats}
 import spray.routing.HttpService
-import org.json4s.{DefaultFormats, Formats}
+import org.json4s._
 import spray.httpx.Json4sSupport
 import scala.concurrent.ExecutionContext.Implicits.global
 
-trait AdminHttpServerRoutes extends HttpService with Json4sSupport {
+object MyFormats extends Json4sSupport{
+  override implicit def json4sFormats: Formats = DefaultFormats
+  implicit val statsMarshaller = json4sMarshaller[SprayStats]
+  implicit val jvalueMarshaller = json4sMarshaller[JValue]
+}
+trait AdminHttpServerRoutes extends HttpService {
   self: AdminHttpServerActor =>
 
-
-  override implicit def json4sFormats: Formats = DefaultFormats
-
-  implicit val statsMarshaller = json4sMarshaller[SprayStats]
+  import MyFormats.statsMarshaller
+  import MyFormats.jvalueMarshaller
 
   val adminRoutes =
     path("admin" / "stats") {
       complete {
-        (httpListener ? Http.GetStats)(1.second).mapTo[SprayStats].map{
-          // this map should not be necessary...
-          value => value
-        }
+        (httpListener ? Http.GetStats)(1.second).mapTo[SprayStats]
       }
     } ~ path("admin" / "metrics") {
       complete {
